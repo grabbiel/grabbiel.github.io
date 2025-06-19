efb9ba417c9ea0d35886deee82cb52b7e998e97e
Fran <90626364+grabbiel@users.noreply.github.com>
2025-06-18 22:28:58 -0700

    video effect

--- js/chifon.js
+++ js/chifon.js
@@ -3,6 +3,11 @@ document.addEventListener("htmx:afterRequest", function (event) {
     document.querySelectorAll(".chifon-block video").forEach((video) => {
       video.addEventListener("ended", function () {
         this.style.opacity = "0.6";
+
+        const playIcon = document.createElement("div");
+        playIcon.className = "chifon-play-icon";
+        playIcon.innerHTML = "▶";
+        this.parentElement.appendChild(playIcon);
       });
     });
   }
