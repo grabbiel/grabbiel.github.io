b214ec441c6175355c3da8bed3f5dca75c213273
grabbiel <fcruzado22@gmail.com>
2025-06-27 12:37:21 -0700

    close game ovelay

--- js/games.js
+++ js/games.js
@@ -3,4 +3,9 @@ document.addEventListener('htmx:afterSwap', function (evt) {
   if (evt.target.id === 'game-content') {
     document.getElementById('game-content').style.display = 'block';
   }
+  document.getElementById('game-content').addEventListener('click', function (e) {
+    if (e.target === this) {
+      this.style.display = 'none';
+    }
+  });
 });
