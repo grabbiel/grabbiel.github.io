0e401bcabddc38c6056bd528e4f5fc12880e4e96
grabbiel <fcruzado22@gmail.com>
2025-06-28 08:50:06 -0700

    game event

--- js/games.js
+++ js/games.js
@@ -1,11 +1,16 @@
+document.addEventListener("htmx:afterRequest", function (event) {
+  const url = event.detail.xhr.responseURL;
+  if (url.includes("/videos-more")) {
+    document.getElementById('game-content').addEventListener('click', function (e) {
+      if (e.target === this) {
+        this.style.display = 'none';
+      }
+    });
+  }
+});
 // Show the game content overlay when HTMX loads content
 document.addEventListener('htmx:afterSwap', function (evt) {
   if (evt.target.id === 'game-content') {
     document.getElementById('game-content').style.display = 'block';
   }
-  document.getElementById('game-content').addEventListener('click', function (e) {
-    if (e.target === this) {
-      this.style.display = 'none';
-    }
-  });
 });
