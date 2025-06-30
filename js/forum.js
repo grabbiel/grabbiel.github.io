228a653d7b70d534e53a20882a07ea3c85c94a2b
grabbiel <fcruzado22@gmail.com>
2025-06-30 10:36:51 -0700

    subject

--- js/forum.js
+++ js/forum.js
@@ -1,10 +1,11 @@
 function submitReply(group, articleId) {
   const token = localStorage.getItem('forumPostingToken');
   const body = document.getElementById('replyBody').value;
+  const subject = document.getElementById('replySubject').value;
 
   fetch('https://server.grabbiel.com/forum/post', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: `token=${token}&group=${group}&reply_to=${articleId}&body=${encodeURIComponent(body)}`
+    body: `token=${token}&group=${group}&reply_to=${articleId}&subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`
   });
 }
