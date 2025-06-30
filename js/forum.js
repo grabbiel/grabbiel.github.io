9952b5cfe3ce2575dca0b741501829cd8d36d106
grabbiel <fcruzado22@gmail.com>
2025-06-29 20:36:35 -0700

    server full

--- js/forum.js
+++ js/forum.js
@@ -2,7 +2,7 @@ function submitReply(group, articleId) {
   const token = localStorage.getItem('forumPostingToken');
   const body = document.getElementById('replyBody').value;
 
-  fetch('/forum/post', {
+  fetch('https://server.grabbiel.com/forum/post', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: `token=${token}&group=${group}&reply_to=${articleId}&body=${encodeURIComponent(body)}`
